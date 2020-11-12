f2b1f7d05647bcec377b27ca7d33fca98fc89930
LSX-s-Software <2682191498@qq.com>
2020-11-12 16:17:24 +0800

    增加树状图，完成加入社团界面及弹窗，增加颜色变量、自定义图标，修改部分样式，优化代码结构

---
+++ src/js/page/main.js
@@ -0,0 +1,87 @@
+var defaultData = [
+    {
+        text: '我的社团',
+        tags: ['4'],
+        icon: "myIcon icon-myClub",
+        state: {
+            selected: true
+        },
+        nodes: [
+            {
+                text: 'Child 1',
+                tags: ['2'],
+                icon: "myIcon icon-club",
+                nodes: [
+                    {
+                        text: 'Grandchild 1',
+                        tags: ['0']
+                    },
+                    {
+                        text: 'Grandchild 2',
+                        tags: ['0']
+                    }
+                ]
+            },
+            {
+                text: 'Child 2',
+                icon: "myIcon icon-club",
+                tags: ['0']
+            }
+        ]
+    },
+    {
+        text: '创建社团',
+        icon: "myIcon icon-createClub"
+    },
+    {
+        text: '加入社团',
+        icon: "myIcon icon-joinClub"
+    }
+];
+var myModal = new MyModal;
+$(document).ready(function () {
+    var myVue = new Vue({
+        el: ".mainViewContainer",
+        data: {
+            show: [true, false, false],
+        },
+        //----------按钮点击事件-------------
+        methods: {
+            joinClub: function () {
+                myModal.show({
+                    template: 2,
+                    state: ["fail", "fail"],
+                    errMsg: ["123", "456"],
+                    clubName: "自强Studio"
+                })
+            }
+        }
+    });
+    $('#tree').treeview({
+        data: defaultData,
+        expandIcon: 'glyphicon myIcon icon-arrow',
+        collapseIcon: 'glyphicon myIcon icon-arrow-down',
+        showBorder: false,
+        emptyIcon: "",
+        color: "#000",
+        selectedBackColor: "var(--themeColor)",
+        backColor: "transparent",
+        onhoverColor: "#4fce9fab",
+        onNodeSelected: function (event, data) {
+            console.log(data)
+            switch (data.icon) {
+                case "myIcon icon-myClub":
+                    myVue.show = [true, false, false]
+                    break;
+                case "myIcon icon-createClub":
+                    myVue.show = [false, true, false]
+                    break;
+                case "myIcon icon-joinClub":
+                    myVue.show = [false, false, true]
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
---
+++ src/utils/myModal/myModal.js
@@ -0,0 +1,55 @@
+var myModalVue;
+$(document).ready(function () {
+    myModalVue = new Vue({
+        el: ".myModal-container",
+        data: {
+            show: false,
+            template: 0,
+            state: ["", ""],
+            errMsg: ["", ""],
+            clubName: ""
+        },
+        mounted() {
+            window.vue = this
+        },
+        methods: {
+            backToMyClub: function () {
+                $('#tree').treeview('selectNode', [0]);
+                this.close()
+            },
+            retry: function () { console.error("Method retry Not Implemented.") },
+            close: function () { myModal.close() }
+        }
+    });
+});
+
+class MyModal {
+    /** 显示弹窗
+    * @param {Object}settings 弹窗设置
+    * @param {[Int]}settings.template 模版序号
+    * @param {[String]}settings.state 状态
+    * @param {String}settings.clubName [模版1][成功]社团名称
+    * @param {[String]}settings.errMsg [失败]错误信息
+    * @param {Function}settings.retry [失败]重试处理方法
+    * @todo 增加动画
+    */
+    show(settings) {
+        console.log(settings)
+        myModalVue.template = settings.template || 0
+        myModalVue.state = settings.state || ["", ""]
+        myModalVue.errMsg = settings.errMsg || ["", ""]
+        myModalVue.clubName = settings.clubName
+        if (settings.retry != undefined) {
+            myModalVue.methods.retry = settings.retry
+        }
+
+        myModalVue.show = true
+    }
+
+    /** 关闭弹窗
+    * @todo 增加动画
+    */
+    close() {
+        myModalVue.show = false
+    }
+}
